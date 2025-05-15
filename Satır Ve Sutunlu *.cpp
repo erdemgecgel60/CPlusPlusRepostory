85b98169a5657ca1f10f01a60d01172798e464d0
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 18:16:00 +0300

    Create Satır Ve Sutunlu *.cpp

---
+++ Satır Ve Sutunlu *.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{    
+
+  
+    int boyut;
+
+    cout << "Sayi : " ;
+
+    cin >> boyut;
+ 
+
+    for (int i = 0; i < boyut; i++)
+    {
+
+         for (int j = 0; j <= i; j++)
+         {
+
+            cout << "*";
+     
+         }
+         
+         cout << endl;
+       
+    }
+
+     
+     
+    
+     
+
+
+
+}
