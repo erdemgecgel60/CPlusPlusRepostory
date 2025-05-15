4c5e44cf07e3796543cac6bf2e5f2aeaaa77eed5
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 18:08:33 +0300

    Create Sayının Tek Veya Çift Olmasını Hesaplama.cpp

---
+++ Sayının Tek Veya Çift Olmasını Hesaplama.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{    
+
+     int sayi;
+
+     cout << "Sayi : ";
+
+     cin >> sayi;
+
+     if (sayi % 2 == 0)
+     {
+
+        cout << "Sayi Çifttir!" << endl;
+        
+     }
+     
+     else 
+     {
+
+        cout << "Sayi Tektir!" << endl;
+
+
+     }
+     
+
+
+
+}
