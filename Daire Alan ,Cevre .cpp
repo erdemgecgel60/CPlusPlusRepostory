7a929d98bf8f6b2a15d97cadfe46ff48f4f70c02
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 19:33:03 +0300

    Create Daire Alan ,Cevre .cpp

---
+++ Daire Alan ,Cevre .cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{
+    
+     float yaricap;
+
+     float pi = 3.14;
+
+     cout << "Yaricap : ";
+
+     cin >> yaricap;
+
+     cout << "Alani : " << pi * (yaricap * yaricap) << endl;
+     cout << "Cevresi : " << 2 * pi * yaricap << endl;
+
+    
+
+
+    
+}
