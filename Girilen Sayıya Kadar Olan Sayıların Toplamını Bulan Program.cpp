1c18747282af9d208b24c738dc324af563787db7
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 18:25:01 +0300

    Create Girilen Sayıya Kadar Olan Sayıların Toplamını Bulan Program.cpp

---
+++ Girilen Sayıya Kadar Olan Sayıların Toplamını Bulan Program.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{    
+
+   
+    int sayi;
+    int toplam = 0;
+
+    cout << "Sayi : ";
+
+    cin >> sayi;
+
+    for (int i = 1; i < sayi; i++)
+    {
+
+
+   
+        toplam += i;
+         
+        
+
+         
+    }
+    
+    cout << "Toplam : " << toplam << endl;
+ 
+
+
+}
