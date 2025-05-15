6a956414a90d5d9e8535c9cc763c88c6d466c9c3
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 18:11:24 +0300

    Create Sayının Pozitif , Negatif Olduğunu Hesaplama.cpp

---
+++ Sayının Pozitif , Negatif Olduğunu Hesaplama.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{    
+
+     int sayi;
+
+     
+
+     cout << "Sayi : ";
+
+     cin >> sayi;
+
+     if (sayi > 0)
+     {
+
+        cout << "Pozitiftir!" << endl;
+       
+     }
+     
+     else if (sayi == 0)
+     {
+
+        cout << "Sayi Notrdir!" << endl;
+    
+     }
+     
+     else 
+     {
+
+        cout << "Sayi Negatiftir!" << endl;
+
+     }
+     
+
+
+
+}
