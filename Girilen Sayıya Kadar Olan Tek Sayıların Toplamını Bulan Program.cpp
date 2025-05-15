2ad2a253309825fe4d6d607c73e895f054e1be73
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-15 18:24:33 +0300

    Create Girilen Sayıya Kadar Olan Tek Sayıların Toplamını Bulan Program.cpp

---
+++ Girilen Sayıya Kadar Olan Tek Sayıların Toplamını Bulan Program.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <math.h> 
+using namespace std;
+int main()
+{    
+
+   
+    int sayi;
+    int toplam = 0;
+
+    cout << "Sayi : ";
+
+    cin >> sayi;
+
+    for (int i = 1; i < sayi; i++)
+    {
+
+
+        if (i % 2 == 0)
+        {
+
+            continue;
+           
+        }
+
+        toplam += i;
+         
+        
+
+         
+    }
+    
+    cout << "Toplam : " << toplam << endl;
+ 
+
+
+}
